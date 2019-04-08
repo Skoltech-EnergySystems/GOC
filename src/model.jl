41bb626677edc3695bd235017ec7d2968d18e5f3
Ilgiz Murzakhanov <30371324+IlgizMurzakhanov@users.noreply.github.com>
2019-04-08 13:17:50 +0300

    Update model.jl

--- src/model.jl
+++ src/model.jl
@@ -79,17 +79,17 @@ function create_model(PN::PNetwork)
     t_g_constr = Array{JuMP.ConstraintRef, 1}() # 5
     diffG = setdiff(PN.caliG, PN.G)
 
-    for g in PN.G
-        i = PN.gen_ind_I[g] # index of generator[g] in the list of all generators
+    sizeG = length(PN.caliG);
+    for i in 1:sizeG # index of generator[g] in the list of all generators
 # 33
-        push!(p_g, @variable(OPF, lower_bound=PN.GeneratorList[i].p_min, upper_bound=PN.GeneratorList[i].p_max, base_name="p_$g"))
+        push!(p_g, @variable(OPF, lower_bound=PN.GeneratorList[i].p_min, upper_bound=PN.GeneratorList[i].p_max, base_name="p_$i"))
 # 35
-        push!(q_g, @variable(OPF, lower_bound=PN.GeneratorList[i].q_min, upper_bound=PN.GeneratorList[i].q_max, base_name="q_$g"))
+        push!(q_g, @variable(OPF, lower_bound=PN.GeneratorList[i].q_min, upper_bound=PN.GeneratorList[i].q_max, base_name="q_$i"))
 # 4, 5
-        push!(t_gh, @variable(OPF, [h=1:PN.GeneratorList[i].N], lower_bound=0, base_name="t_$g"))
+        push!(t_gh, @variable(OPF, [h=1:PN.GeneratorList[i].N], lower_bound=0, base_name="t_$i"))
         push!(t_g_constr, @constraint(OPF, sum(t_gh[end]) == 1))
 # 34, 36
-        if g in diffG
+        if i in diffG
             push!(p_g_constr, @constraint(OPF, p_g[end] == 0))
             push!(q_g_constr, @constraint(OPF, q_g[end] == 0))
         else
@@ -97,7 +97,7 @@ function create_model(PN::PNetwork)
             push!(p_g_constr, @constraint(OPF, p_g[end] == PN.GeneratorList[i].p' * t_gh[end]))
         end
 # 2
-        push!(c_g, @variable(OPF, base_name="c_$g"))
+        push!(c_g, @variable(OPF, base_name="c_$i"))
         push!(c_g_constr, @constraint(OPF, c_g[end] == PN.GeneratorList[i].c' * t_gh[end]))
     end
 
@@ -118,8 +118,8 @@ function create_model(PN::PNetwork)
         push!(v_i, @variable(OPF, lower_bound=PN.BusList[i].v_min, upper_bound=PN.BusList[i].v_max, base_name="v_$i"))
 
 # 37
-        if i in keys(PN.get_shunt_index)
-            push!(bCS_i, @variable(OPF, lower_bound=PN.sShuntList[PN.get_shunt_index[i]].bCS_min, upper_bound=PN.sShuntList[PN.get_shunt_index[i]].bCS_max, base_name="bCS_$(PN.get_shunt_index[i])"))
+        if i in keys(PN.get_sShunt_index)
+            push!(bCS_i, @variable(OPF, lower_bound=PN.sShuntList[PN.get_sShunt_index[i]].bCS_min, upper_bound=PN.sShuntList[PN.get_sShunt_index[i]].bCS_max, base_name="bCS_$(PN.get_sShunt_index[i])"))
         end
 # 47 48 50 51
         push!(sigPp_i, @variable(OPF, lower_bound=0, base_name="sigPp_$i") )
@@ -147,22 +147,22 @@ function create_model(PN::PNetwork)
     qD_e_constr = Array{JuMP.ConstraintRef, 1}()
     # ∀ e ∈ E
     for L in PN.LineList
-    push!(pO_e, @variable(OPF, base_name="pO_e_$(L.e)"))
-    push!(qO_e, @variable(OPF, base_name="qO_e_$(L.e)"))
-    push!(pD_e, @variable(OPF, base_name="pD_e_$(L.e)"))
-    push!(qD_e, @variable(OPF, base_name="qD_e_$(L.e)"))
-    # 38
-    push!(pO_e_constr, @NLconstraint(OPF,
-    pO_e[end] == L.g * v_i[L.iO]^2 + (-L.g * cos(theta_i[L.iO] - theta_i[L.iD]) - L.b * sin(theta_i[L.iO] - theta_i[L.iD])) * v_i[L.iO] * v_i[L.iD] ))
-    # 39
-    push!(qO_e_constr, @NLconstraint(OPF,
-    qO_e[end] == -(L.b + L.bCH/2) * v_i[L.iO]^2 + (L.b * cos(theta_i[L.iO] - theta_i[L.iD]) - L.g * sin(theta_i[L.iO] - theta_i[L.iD])) * v_i[L.iO] * v_i[L.iD] ))
-    # 40
-    push!(pD_e_constr, @NLconstraint(OPF,
-    pD_e[end] == L.g * v_i[L.iD]^2 + (-L.g * cos(theta_i[L.iD] - theta_i[L.iO]) - L.b * sin(theta_i[L.iD] - theta_i[L.iO])) * v_i[L.iO] * v_i[L.iD] ))
-    # 41
-    push!(qD_e_constr, @NLconstraint(OPF,
-    qD_e[end] == -(L.b + L.bCH/2) * v_i[L.iD]^2 + (L.b * cos(theta_i[L.iD] - theta_i[L.iO]) - L.g * sin(theta_i[L.iD] - theta_i[L.iO])) * v_i[L.iO] * v_i[L.iD] ))
+        push!(pO_e, @variable(OPF, base_name="pO_e_$(L.e)"))
+        push!(qO_e, @variable(OPF, base_name="qO_e_$(L.e)"))
+        push!(pD_e, @variable(OPF, base_name="pD_e_$(L.e)"))
+        push!(qD_e, @variable(OPF, base_name="qD_e_$(L.e)"))
+        # 38
+        push!(pO_e_constr, @NLconstraint(OPF,
+        pO_e[end] == L.g * v_i[L.iO]^2 + (-L.g * cos(theta_i[L.iO] - theta_i[L.iD]) - L.b * sin(theta_i[L.iO] - theta_i[L.iD])) * v_i[L.iO] * v_i[L.iD] ))
+        # 39
+        push!(qO_e_constr, @NLconstraint(OPF,
+        qO_e[end] == -(L.b + L.bCH/2) * v_i[L.iO]^2 + (L.b * cos(theta_i[L.iO] - theta_i[L.iD]) - L.g * sin(theta_i[L.iO] - theta_i[L.iD])) * v_i[L.iO] * v_i[L.iD] ))
+        # 40
+        push!(pD_e_constr, @NLconstraint(OPF,
+        pD_e[end] == L.g * v_i[L.iD]^2 + (-L.g * cos(theta_i[L.iD] - theta_i[L.iO]) - L.b * sin(theta_i[L.iD] - theta_i[L.iO])) * v_i[L.iO] * v_i[L.iD] ))
+        # 41
+        push!(qD_e_constr, @NLconstraint(OPF,
+        qD_e[end] == -(L.b + L.bCH/2) * v_i[L.iD]^2 + (L.b * cos(theta_i[L.iD] - theta_i[L.iO]) - L.g * sin(theta_i[L.iD] - theta_i[L.iO])) * v_i[L.iO] * v_i[L.iD] ))
     end
 
     # Transformer flow: 42 - 45
@@ -179,62 +179,75 @@ function create_model(PN::PNetwork)
     #
     # ∀ f ∈ F
     for T in PN.TransformerList
-    push!(pO_f, @variable(OPF, base_name="pO_f_$(T.f)"))
-    push!(qO_f, @variable(OPF, base_name="qO_f_$(T.f)"))
-    push!(pD_f, @variable(OPF, base_name="pD_f_$(T.f)"))
-    push!(qD_f, @variable(OPF, base_name="qD_f_$(T.f)"))
-    #
-    # 42
-    push!(pO_f_constr, @NLconstraint(OPF,
-    pO_f[end] == (T.g / T.tau^2 + T.gM) * v_i[T.iO]^2 + (-T.g / T.tau * cos(theta_i[T.iO] - theta_i[T.iD]  - T.theta) - T.b / T.tau * sin(theta_i[T.iO] - theta_i[T.iD])) * v_i[T.iO] * v_i[T.iD] ))
-    # 43
-    push!(qO_f_constr, @NLconstraint(OPF,
-    qO_f[end] == -(T.b / T.tau^2 + T.bM) * v_i[T.iO]^2 + (T.b / T.tau * cos(theta_i[T.iO] - theta_i[T.iD]) - T.g / T.tau * sin(theta_i[T.iO] - theta_i[T.iD])) * v_i[T.iO] * v_i[T.iD] ))
-    # 44
-    push!(pD_f_constr, @NLconstraint(OPF,
-    pD_f[end] == T.g * v_i[T.iD]^2 + (-T.g / T.tau * cos(theta_i[T.iD] - theta_i[T.iO]) - T.b / T.tau * sin(theta_i[T.iD] - theta_i[T.iO])) * v_i[T.iO] * v_i[T.iD] ))
-    # 45
-    push!(qD_f_constr, @NLconstraint(OPF,
-    qD_f[end] == -T.b * v_i[T.iD]^2 + (T.b / T.tau * cos(theta_i[T.iD] - theta_i[T.iO]) - T.g / T.tau * sin(theta_i[T.iD] - theta_i[T.iO])) * v_i[T.iO] * v_i[T.iD] ))
+        push!(pO_f, @variable(OPF, base_name="pO_f_$(T.f)"))
+        push!(qO_f, @variable(OPF, base_name="qO_f_$(T.f)"))
+        push!(pD_f, @variable(OPF, base_name="pD_f_$(T.f)"))
+        push!(qD_f, @variable(OPF, base_name="qD_f_$(T.f)"))
+        #
+        # 42
+        push!(pO_f_constr, @NLconstraint(OPF,
+        pO_f[end] == (T.g / T.tau^2 + T.gM) * v_i[T.iO]^2 + (-T.g / T.tau * cos(theta_i[T.iO] - theta_i[T.iD]  - T.theta) - T.b / T.tau * sin(theta_i[T.iO] - theta_i[T.iD])) * v_i[T.iO] * v_i[T.iD] ))
+        # 43
+        push!(qO_f_constr, @NLconstraint(OPF,
+        qO_f[end] == -(T.b / T.tau^2 + T.bM) * v_i[T.iO]^2 + (T.b / T.tau * cos(theta_i[T.iO] - theta_i[T.iD]) - T.g / T.tau * sin(theta_i[T.iO] - theta_i[T.iD])) * v_i[T.iO] * v_i[T.iD] ))
+        # 44
+        push!(pD_f_constr, @NLconstraint(OPF,
+        pD_f[end] == T.g * v_i[T.iD]^2 + (-T.g / T.tau * cos(theta_i[T.iD] - theta_i[T.iO]) - T.b / T.tau * sin(theta_i[T.iD] - theta_i[T.iO])) * v_i[T.iO] * v_i[T.iD] ))
+        # 45
+        push!(qD_f_constr, @NLconstraint(OPF,
+        qD_f[end] == -T.b * v_i[T.iD]^2 + (T.b / T.tau * cos(theta_i[T.iD] - theta_i[T.iO]) - T.g / T.tau * sin(theta_i[T.iD] - theta_i[T.iO])) * v_i[T.iO] * v_i[T.iD] ))
     end
 
-#
+    # ∀ e ∈ E
+    # Line current ratings: 52-54
+    sigS_e = Array{JuMP.VariableRef, 1}()
+    CurrentO_e_constr = Array{JuMP.ConstraintRef, 1}()
+    CurrentD_e_constr = Array{JuMP.ConstraintRef, 1}()
+    for L in PN.LineList
+        # 53
+        push!(sigS_e, @variable(OPF, lower_bound=0, base_name="sigS_$(L.e)") )
+        # 52
+        push!(CurrentO_e_constr, @NLconstraint(OPF, sqrt(pO_e[end]^2 + qO_e[end]^2) ≤ L.R_max*v_i[L.iO] + sigS_e[end]))
+        # 54
+        push!(CurrentD_e_constr, @NLconstraint(OPF, sqrt(pD_e[end]^2 + qD_e[end]^2) ≤ L.R_max*v_i[L.iD] + sigS_e[end]))
+    end
+
+    # ∀ f ∈ F
+    # Transformer power ratings: 55-57
+    sigS_f = Array{JuMP.VariableRef, 1}()
+    PowerO_f_constr = Array{JuMP.ConstraintRef, 1}()
+    PowerD_f_constr = Array{JuMP.ConstraintRef, 1}()
+    for T in PN.TransformerList
+        # 56
+        push!(sigS_f, @variable(OPF, lower_bound=0, base_name="sigS_$(T.f)") )
+        # 55
+        push!(PowerO_f_constr, @NLconstraint(OPF, sqrt(pO_f[end]^2 + qO_f[end]^2) <= T.s_max + sigS_f[end]))
+        # 57
+        push!(PowerD_f_constr, @NLconstraint(OPF, sqrt(pD_f[end]^2 + qD_f[end]^2) <= T.s_max + sigS_f[end]))
+    end
+
     # Nodal equations
     # 46
-    sizeG = maximum(PN.caliG);
     pNod_constr = Array{JuMP.ConstraintRef, 1}()
-    # How to make G, Ld, D, fS, L, T - global variables?
     for i = 1:sizeI
         push!(pNod_constr, @NLconstraint(OPF,
-        sum(p_g[g] for g in G.id) - Ld.pL[B.i] - fS.gFS[B.i]*v_i[B.i]^2 - sum(pO_e[m] for m in L.iO) - sum(pD_e[n] for n in L.iD)
-        - sum(pO_f[x] for x in T.iO)- sum(pD_f[y] for y in T.iD) == sigPp_i[B.i] - sigPm_i[B.i]))
+            sum(p_g[g] for g in 1:sizeG if PN.GeneratorList[g].i==i) - sum(PN.LoadList[l].pL for l in 1:length(PN.LoadList) if PN.LoadList[l].i==i)
+            #- sum(PN.fShuntList[f].gFS*v_i[f]^2 for f in 1:length(PN.fShuntList) if PN.fShuntList[f].i==i) # no fixed shunts in Challenge 1
+            - sum(pO_e[oe] for oe in 1:length(PN.LineList) if PN.LineList[oe].iO==i) - sum(pD_e[de] for de in 1:length(PN.LineList) if PN.LineList[de].iD==i)
+            - sum(pO_f[of] for of in 1:length(PN.TransformerList) if PN.TransformerList[of].iO==i)
+            - sum(pD_f[df] for df in 1:length(PN.TransformerList) if PN.TransformerList[df].iD==i)
+            == sigPp_i[i] - sigPm_i[i]))
     end
 
-    #= This loop even for short equation takes a lot of time
-    for G in PN.GeneratorList
-        for Ld in PN.LoadList
-            for B in PN.BusList
-                #for fS in PN.fShuntList
-                for L in PN.LineList
-                    for T in PN.TransformerList
-                        for i = 1:sizeI
-                            push!(pNod_constr, @NLconstraint(OPF,
-                            sum(p_g[g] for g in G.id) == sigPp_i[B.i] - sigPm_i[B.i]))
-                        end
-                    end
-                end
-            end
-        end
-    end
-    =#
-
-#=
     # 49
     qNod_constr = Array{JuMP.ConstraintRef, 1}()
     for i = 1:sizeI
         push!(qNod_constr, @NLconstraint(OPF,
-        sum(q_g[g] for g in G.id) - Ld.ql[B.i] - (- fS.bFS[B.i] - sS.bCS0[B.i])*v_i[B.i]^2 - sum(qO_e[m] for m in L.iO) - sum(qD_e[n] for n in L.iD)
-        - sum(qO_f[x] for x in T.iO)- sum(qD_f[y] for y in T.iD) == sigQp_i[B.i] - sigQm_i[B.i]))
+            sum(q_g[g] for g in 1:sizeG if PN.GeneratorList[g].i==i) - sum(PN.LoadList[l].qL for l in 1:length(PN.LoadList) if PN.LoadList[l].i==i)
+            - sum(-PN.sShuntList[s].bCS0*v_i[s]^2 for s in 1:length(PN.sShuntList) if PN.sShuntList[s].i==i) # no fixed shunts in Challenge 1
+            - sum(qO_e[oe] for oe in 1:length(PN.LineList) if PN.LineList[oe].iO==i) - sum(qD_e[de] for de in 1:length(PN.LineList) if PN.LineList[de].iD==i)
+            - sum(qO_f[of] for of in 1:length(PN.TransformerList) if PN.TransformerList[of].iO==i)
+            - sum(qD_f[df] for df in 1:length(PN.TransformerList) if PN.TransformerList[df].iD==i)
+            == sigQp_i[i] - sigQm_i[i]))
     end
-    =#
 end
